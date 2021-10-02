b8abd5fdbdd191a248ee953bd5959a941f484e6a
AlexanderChen1989 <alexanderchenmh@gmail.com>
2021-10-03 05:41:29 +0800

    update

--- construct/main.cc
+++ construct/main.cc
@@ -31,19 +31,69 @@ public:
 		*(this->v) = *(other.v) + 100;
 	}
 
-	~Hello()
+	Hello(Hello &&other)
 	{
 		std::cout
-			<< "Drop Hello "
-			<< *(this->v)
+			<< "Move Construct "
+			<< std::endl;
+
+		assert(this->v == nullptr);
+		this->v = other.v;
+		other.v = nullptr;
+	}
+
+	~Hello()
+	{
+		if (!this->v)
+			return;
+		std::cout
+			<< "Free "
+			<< this->v
 			<< std::endl;
 		free(this->v);
+	}
+
+	Hello &operator=(const Hello &other)
+	{
+
+		std::cout
+			<< "Copy Assign "
+			<< this->v
+			<< " "
+			<< other.v
+			<< std::endl;
+
+		*(this->v) = *(other.v);
+		return *this;
+	}
+
+	Hello &operator=(Hello &&other)
+	{
+		std::cout
+			<< "Move Assign "
+			<< std::endl;
+
+		if (this->v)
+		{
+			std::cout
+				<< "Free "
+				<< this->v
+				<< " Move "
+				<< other.v
+				<< std::endl;
+			free(this->v);
+		}
+		this->v = other.v;
+		other.v = nullptr;
+		return *this;
 	}
 
 	void sayHello()
 	{
 		std::cout
 			<< "Hello from "
+			<< this->v
+			<< " "
 			<< *(this->v)
 			<< std::endl;
 	}
@@ -54,19 +104,61 @@ private:
 
 int main()
 {
-	Hello h;
-	h.sayHello();
 
-	// copy construct
-	Hello h1 = h;
-	h1.sayHello();
-	Hello h2(h);
-	h2.sayHello();
-	Hello h3{h};
-	h3.sayHello();
+	{
+		std::cout
+			<< "====== copy construct ========="
+			<< std::endl;
+		Hello h;
+		h.sayHello();
+
+		// copy construct
+		Hello h1 = h;
+		h1.sayHello();
+		Hello h2(h);
+		h2.sayHello();
+		Hello h3{h};
+		h3.sayHello();
+	}
 
 	// copy assign
-	
+	{
+		std::cout
+			<< "====== copy assign ========="
+			<< std::endl;
+
+		Hello h;
+		h.sayHello();
+		Hello h1;
+		Hello h2;
+		h2 = h1 = h;
+		h1.sayHello();
+	}
+
+	// move construct
+	{
+		std::cout
+			<< "====== move construct ========="
+			<< std::endl;
+
+		Hello h;
+		h.sayHello();
+		Hello h1 = std::move(h);
+		h1.sayHello();
+		Hello h2(std::move(h1));
+		h2.sayHello();
+		Hello h3{std::move(h2)};
+		h3.sayHello();
+	}
+
+	// move assign
+	{
+		std::cout
+			<< "====== move assign ========="
+			<< std::endl;
+		Hello h, h2, h3;
+		h = std::move(h2 = std::move(h3));
+	}
 
 	return 0;
 }
